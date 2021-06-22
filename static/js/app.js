3d868e527801f4b094826686238fa3c38a6f1621
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-22 19:51:38 -0400

    Show All data and clear table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Load Table
+
+function LoadDataFromJSON() { 
+    ResetTable()
+    var table = document.getElementById("ufo-table");
+    data.forEach(element => {
+        tr = table.insertRow(-1);
+        console.log(element.length);
+        Object.keys(element).forEach(key => {
+            var thisCell = tr.insertCell(-1);
+            thisCell.innerHTML = element[key];
+        });
+    });
+
+};
+
+function ResetTable() {
+    var table = document.getElementById("ufo-table");
+    var oldBody = document.getElementById("ufo-table-body");
+    var newBody = document.createElement("tbody")
+    table.replaceChild(newBody, oldBody);
+    newBody.setAttribute("id", "ufo-table-body")
+}
