36bebed788d33727e7064719e876cc621cc553c4
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-22 22:55:59 -0400

    create function to dynamically fill list options

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,53 @@
 // from data.js
 var tableData = data;
 
-// Load Table
+// Get unique values from data
+var dates = [...new Set (data.map(element => element.datetime))];
+var cities = [...new Set (data.map(element => element.city))];
+var states = [...new Set (data.map(element => element.state))];
+var countries = [...new Set (data.map(element => element.country))];
+var shapes = [...new Set (data.map(element => element.shape))];
 
+// Add dropdown Items
+var cityList = document.getElementById("city-list");
+
+function addLabels(listArray, listNodeID, name) {
+    var i = 1;
+    var listNode = document.getElementById(listNodeID);
+    listArray.forEach(element => {
+        // Create the "input" item
+        var thisInput = document.createElement("input");
+        // Add attributes
+        thisInput.setAttribute("class", "dropdown-item");
+        thisInput.setAttribute("type", "checkbox");
+        thisInput.setAttribute("value", element);
+        thisInput.setAttribute("name", `${name}_${i}`);
+        // Create the "label" item
+        var thisLabel = document.createElement("label");
+        thisLabel.setAttribute("for", `${name}_${i}`);
+        thisLabel.innerHTML = element;
+        // Create the line break
+        var thisBr = document.createElement("br")
+        // Append children to parent node
+        listNode.appendChild(thisInput);
+        listNode.appendChild(thisLabel);
+        listNode.appendChild(thisBr);
+        // increment name suffix
+        i ++;
+
+
+    })
+}
+
+
+
+// Load Table
 function LoadDataFromJSON() { 
     // Clear output table
     ResetTable()
     // Get Date and transform into date obj
     let dateTimeArray = document.getElementById("datetime").value.split("-");
     let dateTime = new Date(dateTimeArray[0], dateTimeArray[1]-1, dateTimeArray[2]);
-    let city = document.getElementById("city").value;
-    let state = document.getElementById("state").value;
-    let country = document.getElementById("country").value;
-    let shape = document.getElementById("shape").value;
-
-    
-    
-    
     let table = document.getElementById("ufo-table-body");
 
     data.forEach(element => {
