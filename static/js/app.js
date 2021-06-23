b347c718c766a30be2ceca2ef75d35efb100b6f3
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-22 20:23:27 -0400

    Multiple form inputs

--- static/js/app.js
+++ static/js/app.js
@@ -4,11 +4,11 @@ var tableData = data;
 // Load Table
 
 function LoadDataFromJSON() { 
+
     ResetTable()
     var table = document.getElementById("ufo-table");
     data.forEach(element => {
         tr = table.insertRow(-1);
-        console.log(element.length);
         Object.keys(element).forEach(key => {
             var thisCell = tr.insertCell(-1);
             thisCell.innerHTML = element[key];
