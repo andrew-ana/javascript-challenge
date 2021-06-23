687d10cf3a805faec24f6584f95edb3151b3f35b
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-22 21:39:55 -0400

    Date Filter Functional

--- static/js/app.js
+++ static/js/app.js
@@ -4,26 +4,26 @@ var tableData = data;
 // Load Table
 
 function LoadDataFromJSON() { 
+    // Clear output table
     ResetTable()
-    let dateTime = document.getElementById("datetime").value;
-    console.log(dateTime);
-    var city = document.getElementById("city").value;
-    var state = document.getElementById("state").value;
-    var country = document.getElementById("city").value;
+    // Get Date and transform into date obj
+    let dateTimeArray = document.getElementById("datetime").value.split("-");
+    let dateTime = new Date(dateTimeArray[0], dateTimeArray[1]-1, dateTimeArray[2]);
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let country = document.getElementById("country").value;
+    let shape = document.getElementById("shape").value;
 
-    //var dateArray = dateTime.split("/");
-    //var dateObj =  new Date(dateTime);
-    //console.log(dateObj);
     
     
     
     let table = document.getElementById("ufo-table-body");
 
     data.forEach(element => {
-        if ( new Date(element["datetime"]) == dateTime) {
+        if ( new Date(element["datetime"]).getTime() == dateTime.getTime()) {
             let tr = table.insertRow(-1);
             Object.keys(element).forEach(key => {
-                var thisCell = tr.insertCell(-1);
+                let thisCell = tr.insertCell(-1);
                 thisCell.innerHTML = element[key];
             });
         };
