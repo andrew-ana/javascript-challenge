e8b3236ca21f17b8982033779d36d5efec53464b
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-22 21:20:15 -0400

    Date Picker Input

--- static/js/app.js
+++ static/js/app.js
@@ -4,23 +4,41 @@ var tableData = data;
 // Load Table
 
 function LoadDataFromJSON() { 
-
     ResetTable()
-    var table = document.getElementById("ufo-table");
-    data.forEach(element => {
-        tr = table.insertRow(-1);
-        Object.keys(element).forEach(key => {
-            var thisCell = tr.insertCell(-1);
-            thisCell.innerHTML = element[key];
-        });
-    });
+    let dateTime = document.getElementById("datetime").value;
+    console.log(dateTime);
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var country = document.getElementById("city").value;
 
+    //var dateArray = dateTime.split("/");
+    //var dateObj =  new Date(dateTime);
+    //console.log(dateObj);
+    
+    
+    
+    let table = document.getElementById("ufo-table-body");
+
+    data.forEach(element => {
+        if ( new Date(element["datetime"]) == dateTime) {
+            let tr = table.insertRow(-1);
+            Object.keys(element).forEach(key => {
+                var thisCell = tr.insertCell(-1);
+                thisCell.innerHTML = element[key];
+            });
+        };
+    });
 };
 
 function ResetTable() {
-    var table = document.getElementById("ufo-table");
-    var oldBody = document.getElementById("ufo-table-body");
-    var newBody = document.createElement("tbody")
+    let table = document.getElementById("ufo-table");
+    let oldBody = document.getElementById("ufo-table-body");
+    let newBody = document.createElement("tbody")
     table.replaceChild(newBody, oldBody);
     newBody.setAttribute("id", "ufo-table-body")
 }
+
+function ResetForm() {
+    document.getElementById("filter-form").reset();
+    ResetTable();
+};
